6165d7fac284657b03c374dd371c6a6f82362b01
AA5544 <aa5544@student.jamk.fi>
2020-10-16 17:42:47 +0200

    ex06 layout + fun sendMessage

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+
+}
